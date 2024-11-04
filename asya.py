2bcc373862ede062ec6ce11016bf823c91e381c2
Yakup <martyrerdixon@gmail.com>
2024-11-05 01:17:57 +0300

    Final!

--- asya.py
+++ asya.py
@@ -3,6 +3,7 @@ init(autoreset=True)
 from time import sleep
 from os import system
 from sms import SendSms
+import sms
 from concurrent.futures import ThreadPoolExecutor, wait
 import threading
 import fade
@@ -51,6 +52,15 @@ kedi1 = r"""
 def titresim():
     titresim_is = threading.Thread(target=system, args=["termux-vibrate -d 3500 -f"])
     titresim_is.start()
+
+
+def asya():
+        print(f"""
+      \    /\       Meow.
+       )  ( ')      Toplam                : {Style.BRIGHT}{sms.toplam}{Style.RESET_ALL}
+      (  /  )       Giden sms sayisi      : {Fore.GREEN}{Style.BRIGHT}{sms.basarili}{Style.RESET_ALL}
+asya   \(__)|       Gidemeyen sms sayisi  : {Fore.RED}{Style.BRIGHT}{sms.basarisiz}{Style.RESET_ALL}
+""")
 
 system("cls||clear")
 print(fade.blackwhite(kedi))
@@ -111,54 +121,128 @@ while 1:
                 with ThreadPoolExecutor() as executor:
                     futures = [
                         executor.submit(send_sms.Akasya),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Akbati),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Ayyildiz),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Baydoner),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Beefull),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Bim),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Bisu),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Bodrum),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Clickme),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Dominos),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Englishhome),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.Evidea),
+                        executor.submit(asya),
+                        executor.submit(asya),
+                        executor.submit(asya),
                         executor.submit(send_sms.File),
+                        executor.submit(asya),
                         executor.submit(send_sms.Frink),
+                        executor.submit(asya),
                         executor.submit(send_sms.Happy),
+                        executor.submit(asya),
                         executor.submit(send_sms.Hayatsu),
+                        executor.submit(asya),
                         executor.submit(send_sms.Hey),
+                        executor.submit(asya),
                         executor.submit(send_sms.Hizliecza),
+                        executor.submit(asya),
                         executor.submit(send_sms.Icq),
+                        executor.submit(asya),
                         executor.submit(send_sms.Ipragaz),
+                        executor.submit(asya),
                         executor.submit(send_sms.Istegelsin),
+                        executor.submit(asya),
                         executor.submit(send_sms.Joker),
+                        executor.submit(asya),
                         executor.submit(send_sms.KahveDunyasi),
+                        executor.submit(asya),
                         executor.submit(send_sms.KimGb),
+                        executor.submit(asya),
                         executor.submit(send_sms.Komagene),
+                        executor.submit(asya),
                         executor.submit(send_sms.Koton),
+                        executor.submit(asya),
                         executor.submit(send_sms.KuryemGelsin),
+                        executor.submit(asya),
                         executor.submit(send_sms.Macro),
+                        executor.submit(asya),
                         executor.submit(send_sms.Metro),
+                        executor.submit(asya),
                         executor.submit(send_sms.Migros),
+                        executor.submit(asya),
                         executor.submit(send_sms.Naosstars),
+                        executor.submit(asya),
                         executor.submit(send_sms.Paybol),
+                        executor.submit(asya),
                         executor.submit(send_sms.Pidem),
+                        executor.submit(asya),
                         executor.submit(send_sms.Porty),
+                        executor.submit(asya),
                         executor.submit(send_sms.Qumpara),
+                        executor.submit(asya),
                         executor.submit(send_sms.Starbucks),
+                        executor.submit(asya),
                         executor.submit(send_sms.Suiste),
+                        executor.submit(asya),
                         executor.submit(send_sms.Taksim),
+                        executor.submit(asya),
                         executor.submit(send_sms.Tasdelen),
+                        executor.submit(asya),
                         executor.submit(send_sms.Tasimacim),
+                        executor.submit(asya),
                         executor.submit(send_sms.Tazi),
+                        executor.submit(asya),
                         executor.submit(send_sms.TiklaGelsin),
+                        executor.submit(asya),
                         executor.submit(send_sms.ToptanTeslim),
+                        executor.submit(asya),
                         executor.submit(send_sms.Uysal),
+                        executor.submit(asya),
                         executor.submit(send_sms.Wmf),
+                        executor.submit(asya),
                         executor.submit(send_sms.Yapp),
+                        executor.submit(asya),
                         executor.submit(send_sms.YilmazTicaret),
-                        executor.submit(send_sms.Yuffi)
+                        executor.submit(asya),
+                        executor.submit(send_sms.Yuffi),
+                        executor.submit(asya)
                     ]
+
+                    
                     wait(futures)
         except KeyboardInterrupt:
             system("cls||clear")
